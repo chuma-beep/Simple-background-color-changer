443a0b583969e42d4ac70449148fcadcc3079aa6
Chukwuma Wisdom Anwaegbu <wisboynelson123@gmail.com>
2023-01-17 09:02:40 +0000

    Changed the transition time.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //image variables 
 var i = 0;
 var image = []; 
-var time = 3000;
+var time = 5000;
 
 //image list 
 image[0] = "car1.jpg";
@@ -20,3 +20,23 @@ if (i < image.length -1){
   setTimeout("changeImg()", time);
 };
 window.onload = changeImg;
+
+// Get all buttons
+var buttons = document.querySelectorAll('.button');
+
+// Convert buttons to array
+var buttonArray = Array.from(buttons);
+
+// Add click event listener to each button
+buttonArray.forEach(function(button) {
+  button.addEventListener('click', function() {
+     // Increment i 
+    if (i < image.length -1){
+      i++;
+    }else{
+      i = 0;
+    }
+    // change image 
+    document.slide.src  =image[i];
+  });
+});
